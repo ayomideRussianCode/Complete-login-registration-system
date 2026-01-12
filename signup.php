3c9e5d0dc7cbeaf6816514aa8a73236db337d9b9
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-01-12 15:11:33 +0100

    form validation put in a reusable function

--- signup.php
+++ signup.php
@@ -1,32 +1,30 @@
 <?php
-
-//database conn script added
-
+//add our database connection script
 include_once 'resource/Database.php';
+include_once 'resource/utilities.php';
 
-//form processing
-
-if (isset($_POST['signup-btn'])) {
-
+//process the form
+if (isset($_POST['signupBtn'])) {
     //initialize an array to store any error message from the form
-
     $form_errors = array();
 
-    //form validation
+    //Form validation
     $required_fields = array('email', 'username', 'password');
 
-    //loop thru the required fields array
-    foreach ($required_fields as $name_of_field) {
-        if (!isset($_POST[$name_of_field]) || $_POST[$name_of_field] == NULL) {
-            $form_errors[] = $name_of_field . " is a required field";
-        }
-    }
+    //call the function to check empty field and merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_empty_fields($required_fields));
+
+    //Fields that requires checking for minimum length
+    $fields_to_check_length = array('username' => 4, 'password' => 6);
+
+    //call the function to check minimum required length and merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_min_length($fields_to_check_length));
+
+    //email validation / merge the return data into form_error array
+    $form_errors = array_merge($form_errors, check_email($_POST));
 
     //check if error array is empty, if yes process form data and insert record
     if (empty($form_errors)) {
-
-
-
         //collect form data and store in variables
         $email = $_POST['email'];
         $username = $_POST['username'];
@@ -34,58 +32,35 @@ if (isset($_POST['signup-btn'])) {
 
         //hashing the password
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
         try {
-            //create sql insert stmt
-            $sql = " INSERT INTO users (username, email, password, join_date)
-    VALUES (:username, :email , :password, now())
-    ";
+
+            //create SQL insert statement
+            $sqlInsert = "INSERT INTO users (username, email, password, join_date)
+              VALUES (:username, :email, :password, now())";
 
             //use PDO prepared to sanitize data
-            $stmt = $db->prepare($sql);
+            $statement = $db->prepare($sqlInsert);
 
-            //insert data into the db
-            $stmt->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
+            //add the data into the database
+            $statement->execute(array(':username' => $username, ':email' => $email, ':password' => $hashed_password));
 
-            //check if new row was created
-            if ($stmt->rowCount() == 1) {
-
-                $message =  "<p style = 'padding: 20px; color: green;' > Registration successful </p>";
+            //check if one new row was created
+            if ($statement->rowCount() == 1) {
+                $result = "<p style='padding:20px; border: 1px solid gray; color: green;'> Registration Successful</p>";
             }
-        } catch (PDOException $e) {
-
-            $message = "<p style = 'padding: 20px; color: red;' > An error occurred: " . $e->getMessage() . "</p>";
+        } catch (PDOException $ex) {
+            $result = "<p style='padding:20px; border: 1px solid gray; color: red;'> An error occurred: " . $ex->getMessage() . "</p>";
         }
     } else {
-    if (count($form_errors) == 1) {
-
-        $message =  "<p style = 'color: red;' > There was 1 error in the form <br>";
-
-        $message .= "<ul style='color:red;'>";
-
-        //loop thru error array and display all items
-        foreach ($form_errors as $error) {
-            $message .= "<li> {$error}</li>";
+        if (count($form_errors) == 1) {
+            $result = "<p style='color: red;'> There was 1 error in the form<br>";
+        } else {
+            $result = "<p style='color: red;'> There were " . count($form_errors) . " errors in the form <br>";
         }
-        $message .=   "</ul></p>";
-    } else {
-
-        $message =  "<p style = 'color: red;'> There were  " . count($form_errors) . " errors in the form <br>";
-
-        $message .= "<ul style='color:red;'>";
-
-        //loop thru error array and display all items
-        foreach ($form_errors as $error) {
-            $message .= "<li>{$error}</li>";
-        }
-        $message .= "</ul></p>";
     }
 }
-}
+
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
 
@@ -96,45 +71,33 @@ if (isset($_POST['signup-btn'])) {
 
 <body>
     <h2>User Authentication System </h2>
+    <hr>
 
     <h3>Registration Form</h3>
 
-
-    <?php
-    if (isset($message)) echo $message;
-    ?>
-    <form method="POST" action="">
+    <?php if (isset($result)) echo $result; ?>
+    <?php if (!empty($form_errors)) echo show_errors($form_errors); ?>
+    <form method="post" action="">
         <table>
             <tr>
-                <td>Email:
+                <td>Email:</td>
                 <td><input type="text" value="" name="email"></td>
-                </td>
             </tr>
             <tr>
-                <td>Username:
+                <td>Username:</td>
                 <td><input type="text" value="" name="username"></td>
-                </td>
             </tr>
-
             <tr>
-                <td>Password:
+                <td>Password:</td>
                 <td><input type="password" value="" name="password"></td>
-                </td>
             </tr>
-
             <tr>
-                <td>
-                <td><input style="float: right;" type="submit" name="signup-btn" value="Sign up"></td>
-                </td>
+                <td></td>
+                <td><input style="float: right;" type="submit" name="signupBtn" value="Signup"></td>
             </tr>
-
-
         </table>
     </form>
-
-    <p> <a href="index.php">Back</a></p>
-
-
+    <p><a href="index.php">Back</a> </p>
 </body>
 
 </html>
