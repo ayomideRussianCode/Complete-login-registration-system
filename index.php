828456996be509746c69ff2e67028938ea16e696
ayomideRussianCode <ayindeayomide7@gmail.com>
2026-01-12 16:01:47 +0100

    sign in done

--- index.php
+++ index.php
@@ -1,22 +1,33 @@
 <?php
 include_once 'resource/Database.php';
+ include_once 'resource/session.php';
 
-?>
+
+    ?>
 
 
 
 <!DOCTYPE html>
 <html>
+
 <head lang="en">
     <meta charset="UTF-8">
     <title>Homepage</title>
 </head>
+
 <body>
-<h2>User Authentication System </h2>
+    <h2>User Authentication System </h2>
 
-<p>You are currently not signed in <a href="login.php">Log in</a> Not yet a member ? <a href="signup.php">Signup</a></p>
-<p>You are logged in as {username} <a href="logout.php">Logout</a></p>
+    <?php if(!isset($_SESSION['username'])): ?>
 
+    <p>You are currently not signed in <a href="login.php">Log in</a> Not yet a member ? <a href="signup.php">Signup</a></p>
+
+    <?php else: ?>
+
+    <p>You are logged in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a></p>
+
+    <?php endif ?>
 
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,5 +1,55 @@
 <?php
 include_once 'resource/Database.php';
+include_once 'resource/session.php';
+include_once 'resource/utilities.php';
+
+if(isset($_POST['login_btn'])){
+    
+
+//array to hold errors
+
+$form_errors = array();
+
+//validate
+$required_fields = array('username', 'password');
+
+$form_errors = array_merge($form_errors, check_empty_fields($required_fields));
+
+if(empty($form_errors)){
+    //collect form data
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //check if user exist in the db
+
+    $sql = " SELECT * FROM users WHERE username = :username";
+    $stmt = $db->prepare($sql);
+    $stmt->execute((array(':username' => $username)));
+
+    while($row = $stmt->fetch()){
+        $id = $row['id'];
+        $hashed_password = $row['password'];
+        $username = $row['username'];
+
+        if(password_verify($password, $hashed_password)){
+
+            $_SESSION['id'] =  $id;
+            $_SESSION['username'] = $username;
+            header("location: index.php");
+        } else {
+            $message = "<p style='padding: 20px; color:red; border: 1px solid gray;'>Invalid username or password</p>";
+        }
+    }
+
+} else {
+    if(count($form_errors ) == 1){
+        $message = "<p style='color:red;'>There was an error in the form</p>";
+    }else {
+        $message =  "<p style='color:red;'>There were " .count($form_errors). " errors in the form</p>";
+    }
+}
+}
 
 ?>
 
@@ -18,23 +68,28 @@ include_once 'resource/Database.php';
 
     <h3>Login Form</h3>
 
+    <?php if(isset($message)) echo $message; ?>
+
+        <?php if(!empty($form_errors)) echo show_errors($form_errors); ?>
+
+
     <form method="post" action="">
         <table>
             <tr>
                 <td>Username:
-                <td><input type="text" value=""></td>
+                <td><input type="text" value="" name="username"></td>
                 </td>
             </tr>
 
             <tr>
                 <td>Password:
-                <td><input type="password" value=""></td>
+                <td><input type="password" value="" name="password"></td>
                 </td>
             </tr>
 
             <tr>
                 <td>
-                <td><input style="float: right;" type="submit" value="Sign in"></td>
+                <td><input style="float: right;" name="login_btn" type="submit" value="Sign in"></td>
                 </td>
             </tr>
 
